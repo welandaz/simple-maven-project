e0963924f24c70769d8e3c9a374a28c87d1ddc79
Iurii Ignatko <iignatko@gradle.com>
2024-07-19 16:01:01 +0200

    Initial commit

---
+++ src/test/java/com/github/welandaz/MainTest.java
@@ -0,0 +1,14 @@
+package com.github.welandaz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    void alwaysPassing() {
+        assertTrue(true);
+    }
+
+}
